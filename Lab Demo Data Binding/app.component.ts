9075702c43db82c5cd90f7dea9ed2dac7ad7298a
Jenna Pangilinan <122793994+yukinon19@users.noreply.github.com>
2024-01-11 14:33:36 +0800

    Add files via upload

---
+++ Lab Demo Data Binding/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_data_binding';
+  clientName = "Jenna Pangilinan"; //property
+  appliedCSS = "green"; //style
+  notAppliedCSS = false;
+  myColor = "red";
+
+  clickCount= 0;
+  clickMe(){
+    this.clickCount++;
+  }
+
+  resetClickCount(){
+    this.clickCount=0;
+  }
+
+}
