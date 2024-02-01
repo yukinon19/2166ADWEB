3ee4392a23b950935b22e1e7aaf3c0af533911cf
Jenna Pangilinan <122793994+yukinon19@users.noreply.github.com>
2024-02-01 16:05:22 +0800

    Add files via upload

---
+++ prelimcode/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  loc1 = 'France';
+  loc2 = 'Seoul';
+  loc3 = 'San Francisco';
+  loc4 = 'Boston';
+  
+}
